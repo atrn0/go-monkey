4cfafeabf48bd3780f87a50b31c86c552f1b4a3b
aratasato <14242642+aratasato@users.noreply.github.com>
2020-01-02 23:13:01 +0900

    add keywords

--- token/token.go
+++ token/token.go
@@ -27,3 +27,15 @@ const (
 	FUNCTION = "FUNCTION"
 	LET      = "LET"
 )
+
+var keywords = map[string]Type{
+	"fn":  FUNCTION,
+	"let": LET,
+}
+
+func LookupIdent(ident string) Type {
+	if tok, ok := keywords[ident]; ok {
+		return tok
+	}
+	return IDENT
+}
