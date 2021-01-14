7d1f8ff9925cd9b5d89b86882f35fcb33d1f1e42
atrn0 <14242642+aratasato@users.noreply.github.com>
2021-01-14 19:37:58 +0900

    update repl to display evaluated integer literal.

--- repl/repl.go
+++ repl/repl.go
@@ -3,6 +3,7 @@ package repl
 import (
 	"bufio"
 	"fmt"
+	"github.com/atrn0/go-monkey/evaluator"
 	"github.com/atrn0/go-monkey/lexer"
 	"github.com/atrn0/go-monkey/parser"
 	"io"
@@ -30,8 +31,11 @@ func Start(in io.Reader, out io.Writer) {
 			continue
 		}
 
-		io.WriteString(out, program.String())
-		io.WriteString(out, "\n")
+		evaluated := evaluator.Eval(program)
+		if evaluated != nil {
+			io.WriteString(out, evaluated.Inspect())
+			io.WriteString(out, "\n")
+		}
 	}
 }
 
